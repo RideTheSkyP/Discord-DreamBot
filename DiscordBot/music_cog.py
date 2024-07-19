a9a181efb6d15e5bcc447e273fcb4782a77e9f29
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-07-19 14:46:27 +0200

    Adjusted ytdl format options and added play_next functionality

--- DiscordBot/music_cog.py
+++ DiscordBot/music_cog.py
@@ -11,23 +11,30 @@ from discord.ext import commands
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'bitrate': 320000,
+    'encoding': 'utf-8',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
+    'socket_timeout': 10,
     'ignoreerrors': False,
     'logtostderr': False,
+    'extract_flat': False,
+    'simulate': True,
+    'prefer_ffmpeg': True,
     'quiet': True,
     # 'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',
 }
-# # yt_opts = { 'format': 'best[ext=mp4]', 'download_ranges': download_range_func(None, [(start_time, end_time)]), 'force_keyframes_at_cuts': True, }
-# #         self.ffmpegOptions = {
-# #             "before_options": f"-ss {self.skipToTime} "
-# #                               f"-reconnect 1 -reconnect_at_eof 1 "
-# #                               f"-reconnect_streamed 1 -reconnect_delay_max 5",
-# #             "options": "-vn"
-# #         }
+
+# yt_opts = { 'format': 'best[ext=mp4]', 'download_ranges': download_range_func(None, [(start_time, end_time)]), 'force_keyframes_at_cuts': True, }
+#         self.ffmpegOptions = {
+#             "before_options": f"-ss {self.skipToTime} "
+#                               f"-reconnect 1 -reconnect_at_eof 1 "
+#                               f"-reconnect_streamed 1 -reconnect_delay_max 5",
+#             "options": "-vn"
+#         }
 
 ffmpeg_options = {
     'options': '-vn',
@@ -47,12 +54,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
     async def from_url(cls, url, *, loop=None, stream=True):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        print('loop', loop)
-        print('data', data)
         if 'entries' in data:
             data = data['entries'][0]
         filename = data['url'] if stream else ytdl.prepare_filename(data)
-        print('filename', filename)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
@@ -88,9 +92,27 @@ class Music(commands.Cog):
     async def leave(self, interaction: discord.Interaction):
         if interaction.guild.voice_client:
             await interaction.guild.voice_client.disconnect(force=True)
-            await interaction.response.send_message('Disconnected from the voice channel.', ephemeral=True)
+            del self.queue[interaction.guild.id]
+            await interaction.response.send_message('Left voice channel', ephemeral=True)
         else:
-            await interaction.response.send_message('I am not in a voice channel.', ephemeral=True)
+            await interaction.response.send_message('Bot is not in a voice channel', ephemeral=True)
+
+    def _add_to_song_queue(self, guild_id, player):
+        if guild_id not in self.queue:
+            self.queue[guild_id] = []
+        self.queue[guild_id].append(player)
+
+    def play_next(self, voice_client):
+        guild_id = voice_client.guild.id
+        if guild_id in self.queue and self.queue[guild_id]:
+            player = self.queue[guild_id].pop(0)
+            voice_client.play(player, after=lambda e: self.bot.loop.create_task(self.play_next(voice_client)) if e is None else print(f'[{voice_client.guild.name}/{voice_client.channel.name}] Player error: {e}'))
+            print(f'[{voice_client.guild.name}|{voice_client.channel.name}] Now playing next song in queue: {player.title}')
+        else:
+            if not voice_client.is_connected() and not (voice_client.is_playing() or voice_client.is_paused()):
+                await asyncio.sleep(150)
+                if not self.queue.get(guild_id) and voice_client.is_connected() and not (voice_client.is_playing() or voice_client.is_paused()):
+                    await voice_client.disconnect()
 
     @app_commands.command(name='play', description='Make the bot play music from query or url')
     async def play(self, interaction: discord.Interaction, query: str):
@@ -99,28 +121,20 @@ class Music(commands.Cog):
         await self._join(interaction)
         guild_id = interaction.guild.id
         voice_client = interaction.guild.voice_client
-        print('query', query)
         player = await YTDLSource.from_url(query, loop=self.bot.loop)
-        print('player', player)
-        print('voice_client', dir(voice_client))
-        print('vars', voice_client)
+
         try:
-            if voice_client or self.queue.get(guild_id):
-                if guild_id not in self.queue:
-                    self.queue[guild_id] = []
-                self.queue[guild_id].append(player)
+            if voice_client.is_connected() and (guild_id in self.queue):
+                self._add_to_song_queue(guild_id, player)
                 await interaction.followup.send(f'Added {player.title} to the queue', ephemeral=False)
+                print(f'[{voice_client.guild.name}|{voice_client.channel.name}] Added {player.title} to the queue')
             else:
-                # if guild_id not in self.played_songs:
-                #     self.played_songs[guild_id] = deque()
-                voice_client.play(player)
-            print('player', player)
+                voice_client.play(player, after=lambda e: self.bot.loop.create_task(self.play_next(voice_client)))
+                await interaction.followup.send_message(f'Playing: {player.title}')
+                print(f'[{voice_client.guild.name}|{voice_client.channel.name}] Playing: {player.title}')
         except Exception as e:
             print(f'Exception: {e}')
             await interaction.response.send_message(f'Error occurred: {e}')
-
-        # voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-        await interaction.response.send_message(f'Now playing: {player.title}')
 
 
 async def setup(bot):
